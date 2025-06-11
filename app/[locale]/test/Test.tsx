62a625ad0437cfa54587b58433b2994be1200fdf
AMIT-COINEARTH <amith.k@coinearth.in>
2025-06-11 11:53:04 +0530

    >

--- app/[locale]/test/Test.tsx
+++ app/[locale]/test/Test.tsx
@@ -1,5 +1,5 @@
 import { motion, AnimatePresence } from "framer-motion";
-import Image from "next/image";
+// import Image from "next/image";
 import { useState, useEffect } from "react";
 
 const images = [
